d7e5710e4073c58dd61bee33fe52a90341b3ebc4
Leon Morten Richter <github@leonmortenrichter.de>
2024-09-29 12:30:36 +0200

    feat: compute crc32c (reverse) suberblock checksum

--- examples/tunefs.py
+++ examples/tunefs.py
@@ -7,6 +7,12 @@ def main():
     with Ext4Filesystem('ext4.img') as fs:
         pprint.pprint(tunefs(fs))
 
+        if fs.checksum_valid:
+            print(f"Superblock checksum is valid (0x{fs.sb.s_checksum:x}).")
+        else:
+            print("!!Corrupted superblock!!")
+            print(f"Invalid checksum: 0x{fs.compute_checksum():x} != 0x{fs.sb.s_checksum:x}")
+
 
 if __name__ == '__main__':
     main()
